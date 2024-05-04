6c371493b865f7ea6f90c800c658f9cd08dc71bc
依栏听风 <83175948+LYX9527@users.noreply.github.com>
2024-05-04 18:39:44 +0800

    Update dm.js

--- dm.js
+++ dm.js
@@ -1,1 +1,9 @@
-
+if ($request.url.indexOf('.starrails.com') != -1||$request.url.indexOf('.hoyoverse.com') != -1||$request.url.indexOf('.mihoyo.com') != -1||$request.url.indexOf('.bhsr.com') != -1) {
+    if($response.status!=200){
+        $done({status: 200, headers: $response.headers, body: '{"cacheExpirationDays":999,"resultCode":"GOOD","message":"Device Valid"}' })
+    }else{
+        $done({})
+    }
+}else{
+    $done({})
+}
